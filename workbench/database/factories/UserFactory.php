a8dbe5bd151a78388f3484bc9f2f3f3d07e96f5a
jdw5 <joshuadwallace1@gmail.com>
2026-05-23 00:08:40 +0000

    feat(abn): bump deps, coverage

--- workbench/database/factories/UserFactory.php
+++ workbench/database/factories/UserFactory.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Workbench\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
--- workbench/database/seeders/DatabaseSeeder.php
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Workbench\Database\Seeders;
 
 use Illuminate\Database\Seeder;
